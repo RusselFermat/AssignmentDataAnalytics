4651a9ff179bad88f74b78a3a2b5f09f6887e76a
RusselFermat <Russel7951@gmail.com>
2025-05-29 15:56:51 +0800

    Initial Commit

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
 
+st.title("""Welcome to my Dashboard
+This is my first time using streamlit.""")
