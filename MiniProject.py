67e5257da776de311079ac4704a47907a41b5584
RusselFermat <Russel7951@gmail.com>
2025-05-29 16:39:00 +0800

    New Packages

--- MiniProject.py
+++ MiniProject.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
+import numpy as np
+import seaborn as sb
 
 st.title("""Welcome to my Dashboard
 This is my first time using streamlit.""")
