f12323e4ec76445c9340f647d48457a88c387e8e
HandTieDev <izzatamir09@gmail.com>
2025-05-30 09:56:49 +0800

    Add Sidebar

--- MiniProject.py
+++ MiniProject.py
@@ -7,3 +7,11 @@ import seaborn as sb
 
 st.title("""Welcome to my Dashboard
 And I don't wanna learn another scent""")
+
+with st.sidebar:
+    st.header("Configuration")
+    api_options = ("echarts", "pyecharts")
+    selected_api = st.selectbox(
+    label="Choose your preferred API:",
+    options=api_options,
+    )
