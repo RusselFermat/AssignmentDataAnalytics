9d9d890b9d63414fbec3017d3d001ce58de53624
HandTieDev <izzatamir09@gmail.com>
2025-06-03 00:02:55 +0800

    Add Animations

--- MiniProject.py
+++ MiniProject.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.snow()
+
 # Set page config
 st.set_page_config(
     page_title="Postgraduate Employability Dashboard", 
